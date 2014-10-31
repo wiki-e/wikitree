a6af702eecd8eddf184f62653ec5ff5f3e4f7e78
ABS <abshetty.192@gmail.com>
2014-10-31 17:01:53 +0530

    wt_finder init
    noob moment#1
    saheb lost phone ragged by a teacher couple
    who were caught having oral sex
    the female was under the desk with his phone in her hands
    and she was surfing on the web on saheb's mobile
    with her sexual activities undisturbed

--- wt_finder.py
+++ wt_finder.py
@@ -1,2 +1,10 @@
-print("hello finder")
+import requests as req
+import sys
+from bs4 import BeautifulSoup
 
+args = sys.argv
+print(args)
+if(len(args) == 2):
+	url = args[1]
+	page = req.get(url);
+	print(page.text.encode('utf-8'))
