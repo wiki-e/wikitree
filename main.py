35453acd114b3f89715d81c4e73ec8c08605d123
ABS <abshetty.192@gmail.com>
2014-10-31 17:35:50 +0530

    Undo Revert "wt_finder init"
    cleaner commit

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import subprocess
 import os
 
+BASE_URL = "http://en.wikipedia.org/wiki/"
 def main():
+	print("\n.: ",end="")
+	read = input()
+	read = BASE_URL+read.replace(' ', '_')
 	if(os.name == "nt"):
-		p = subprocess.Popen("python wt_finder.py",shell=True)
+		p = subprocess.Popen("python wt_finder.py " +read,shell=True)
 	else:
-		p = subprocess.Popen("python34 wt_finder.py",shell=True)
+		p = subprocess.Popen("python34 wt_finder.py "+read,shell=True)
 	p.wait()
-
 
 if(__name__ == "__main__"):
 	main()
